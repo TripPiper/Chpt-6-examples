c457e9a3c24b3e50af174756ffc9104e02858558
Trip <tripiphone4@gmail.com>
2016-02-06 02:25:27 -0500

    Create LengthDemo.java

---
+++ LengthDemo.java
@@ -0,0 +1,19 @@
+
+public class Rectangle {
+    
+// Privates fields can be accesses from within the class
+    private double length;  
+    private double width;
+    
+    /**
+     setLength method stores a value in the  length field.
+      @param len The value to store in length
+      */
+     
+    public void setLength(double len)
+    { 
+        length=len;
+    }
+
+    
+}
