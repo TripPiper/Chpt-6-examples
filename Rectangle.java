eada91cb33c50596a5b205ea6e63803adc55d938
Trip <tripiphone4@gmail.com>
2016-02-06 02:25:07 -0500

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,23 @@
+
+public static void main(String[] args) 
+    {
+         // Creates a Rectangle object and assign its
+        // address to the box variable.
+        Rectangle box =new Rectangle();
+        
+        // Indicates what we are doing.
+        System.out.println( "Sending the value 10.0 " +
+                "to the setLength method");
+        
+        // Indicates what we are done
+        System.out.println( "Done.");
+        
+     
+    }
+
+
+
+
+
+
+
