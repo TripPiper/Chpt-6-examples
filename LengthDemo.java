6505471098a0eaffbf2f29ca9d3f6e4328a03b38
Trip <tripiphone4@gmail.com>
2016-02-06 03:01:34 -0500

    Update LengthDemo.java

--- LengthDemo.java
+++ LengthDemo.java
@@ -1,19 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter6classesandobjects;
 
+/**
+ *
+ * @author Tutor
+ */
 public class Rectangle {
-    
+
 // Privates fields can be accesses from within the class
-    private double length;  
+    private double length;
     private double width;
-    
+
     /**
-     setLength method stores a value in the  length field.
-      @param len The value to store in length
-      */
-     
-    public void setLength(double len)
-    { 
-        length=len;
+     * setLength method stores a value in the length field.
+     *
+     * @param len The value to store in length
+     */
+    public void setLength(double len) {
+        length = len;
     }
 
-    
+    /**
+     * setwidth method stores a value in the width field.
+     *
+     * @param len The value to store in length
+     */
+    public void setWidth(double w) {
+        width = w;
+    }
+
+    public double getLength() {
+
+        //size=box.setLength();
+        return length;
+    }
+
+    public double getWidth() {
+
+        // size=box.setWidth();
+        return width;
+    }
+
+    public double getArea() {
+
+        return length * width;
+    }
+
 }
